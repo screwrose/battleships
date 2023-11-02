ddf4f7289ada76a838f76cf07c67aef898fb2bfd
screwrose <narsas@abv.bg>
2023-11-02 14:44:58 +0200

    +comments

--- battleships.cpp
+++ battleships.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-const int BOARD_SIZE = 5;  // Size of the game board
+const int BOARD_SIZE = 5;  //size of the game board
 
 class BattleshipBoard 
 {
 private:
     
-    vector<pair<int, int>> ships[3];
-    int shipSizes[3] = {4, 3, 2};
-    int lifePoints = 4 + 3 + 2;
+    vector<pair<int, int>> ships[3]; //stores ship placement
+    int shipSizes[3] = {4, 3, 2};   //ship sizing
+    int lifePoints = 4 + 3 + 2;    //used for determining winner
     int playerNumber;
 
 public:
@@ -22,17 +22,18 @@ public:
         playerNumber = _playerNumber;
     }
 
-    char board[BOARD_SIZE][BOARD_SIZE];
-    char FogOfWarBoard[BOARD_SIZE][BOARD_SIZE];
+    char board[BOARD_SIZE][BOARD_SIZE];         //main game board
+    char FogOfWarBoard[BOARD_SIZE][BOARD_SIZE];//used to display where the player shoudl attack
 
-    void initializeBoard(char someboard[BOARD_SIZE][BOARD_SIZE]);
-    void displayBoard(char someboard[BOARD_SIZE][BOARD_SIZE]);
-    void placeShips();
-    bool isHit(int x, int y, char board[BOARD_SIZE][BOARD_SIZE]);
-    void playTurn(char board[BOARD_SIZE][BOARD_SIZE]);
-    bool isDead();
+    void initializeBoard(char someboard[BOARD_SIZE][BOARD_SIZE]); //fills board with '~'
+    void displayBoard(char someboard[BOARD_SIZE][BOARD_SIZE]);   //outputs the board
+    void placeShips(); //ship placement
+    bool isHit(int x, int y, char board[BOARD_SIZE][BOARD_SIZE]); //checks if attacked square is a ship
+    void playTurn(char board[BOARD_SIZE][BOARD_SIZE]); //performs a singular attack
+    bool isDead(); //checks lifePoints
 };
 
+//fills board with '~'
 void BattleshipBoard::initializeBoard(char someboard[BOARD_SIZE][BOARD_SIZE]) 
 {
     for (int i = 0; i < BOARD_SIZE; i++) 
@@ -44,6 +45,7 @@ void BattleshipBoard::initializeBoard(char someboard[BOARD_SIZE][BOARD_SIZE])
     }
 }
 
+//outputs the board
 void BattleshipBoard::displayBoard(char someboard[BOARD_SIZE][BOARD_SIZE]) 
 {
     for (int i = 0; i < BOARD_SIZE; i++) 
@@ -56,6 +58,7 @@ void BattleshipBoard::displayBoard(char someboard[BOARD_SIZE][BOARD_SIZE])
     }
 }
 
+//ship placement
 void BattleshipBoard::placeShips() 
 {
     for (int i = 0; i < 3; i++) 
@@ -67,6 +70,7 @@ void BattleshipBoard::placeShips()
         {
             int x, y;
             cout << "Enter coordinates (x,y) for unit [" << j + 1 << "] (indices start from 0,0 to 4,4): ";
+            //inverting the order of x and y, since it's easier for me to think of a coordinate system than rows/columns
             cin >> y >> x;
 
             ships[i].push_back({x, y});
@@ -79,11 +83,13 @@ void BattleshipBoard::placeShips()
     }
 }
 
+//checks if attacked square is a ship
 bool BattleshipBoard::isHit(int x, int y, char board[BOARD_SIZE][BOARD_SIZE]) 
 {
     return board[x][y] == 'S';
 }
 
+//performs a singular attack
 void BattleshipBoard::playTurn(char board[BOARD_SIZE][BOARD_SIZE]) 
 {
 
@@ -92,6 +98,7 @@ void BattleshipBoard::playTurn(char board[BOARD_SIZE][BOARD_SIZE])
     while(!validCords) 
     {
         cout << "Enter target coordinates (row and column): ";
+        //inverting the order of x and y, since it's easier for me to think of a coordinate system rather than rows/columns
         cin >> y >> x;
 
         if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) 
@@ -108,14 +115,14 @@ void BattleshipBoard::playTurn(char board[BOARD_SIZE][BOARD_SIZE])
     {
         cout << "It's a hit!" << endl;
         board[x][y] = 'X';
-        FogOfWarBoard[x][y] = 'X';
-        lifePoints--;
+        FogOfWarBoard[x][y] = 'X'; //reflects on FogOfWar
+        lifePoints--; //reduces lifepoints so that the if statement in main can break the while loop
     } 
     else 
     {
         cout << "Missed!" << endl;
         board[x][y] = 'O';
-        FogOfWarBoard[x][y] = 'O';
+        FogOfWarBoard[x][y] = 'O'; //reflects on FogOfWar
     }
 
 }
@@ -135,6 +142,7 @@ int main() {
     cout << "Player 2, place your ships:" << endl;
     player2.placeShips();
 
+    //loops until one of the if statements becomes true, returning a break and thus ending the game
     while (true) 
     {
         //NOTICE: playTurn lowers the current player's lifePoints, that's why the numeration in win scenarios is swapped
