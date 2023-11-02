bb6812d01053b6153c7e55bcb8c1229f658475de
screwrose <narsas@abv.bg>
2023-11-02 12:17:56 +0200

    real

---
+++ battleships.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int BOARD_SIZE = 5;  // Size of the game board
+
+class BattleshipBoard 
+{
+private:
+    char board[BOARD_SIZE][BOARD_SIZE];
+    vector<pair<int, int>> ships[3];
+    int shipSizes[3] = {4, 3, 2};
+    int lifePoints = 4 + 3 + 2;
+    int playerNumber;
+
+public:
+    BattleshipBoard(int _playerNumber) 
+    { 
+        initializeBoard();
+        playerNumber = _playerNumber;
+    }
+
+    void initializeBoard();
+    void displayBoard();
+    void placeShips();
+    bool isHit(int x, int y);
+    void playTurn();
+    bool isDead();
+};
+
+void BattleshipBoard::initializeBoard() 
+{
+    for (int i = 0; i < BOARD_SIZE; i++) 
+    {
+        for (int j = 0; j < BOARD_SIZE; j++) 
+        {
+            board[i][j] = '~';
+        }
+    }
+}
+
+void BattleshipBoard::displayBoard() 
+{
+    for (int i = 0; i < BOARD_SIZE; i++) 
+    {
+        for (int j = 0; j < BOARD_SIZE; j++) 
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void BattleshipBoard::placeShips() 
+{
+    for (int i = 0; i < 3; i++) 
+    {
+        int size = shipSizes[i];
+        cout << "Player " << playerNumber << ", place your " << size << "-unit ship (row and column):" << endl;
+
+        for (int j = 0; j < size; j++) 
+        {
+            int x, y;
+            cout << "Enter coordinates (x,y) for unit [" << j + 1 << "] (indices start from 0,0 to 4,4): ";
+            cin >> y >> x;
+
+            ships[i].push_back({x, y});
+            board[x][y] = 'S';
+        }
+
+        cout << "Player " << playerNumber << "'s board after placing the ship:" << endl;
+        displayBoard();
+
+    }
+}
+
+bool BattleshipBoard::isHit(int x, int y) 
+{
+    return board[x][y] == 'S';
+}
+
+void BattleshipBoard::playTurn() 
+{
+
+    int x, y;
+    bool validCords = false;
+    while(!validCords) 
+    {
+        cout << "Enter target coordinates (row and column): ";
+        cin >> y >> x;
+
+        if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) 
+        {
+            cout << "Invalid coordinates. Try again." << endl;
+        }
+        else 
+        {
+            validCords = true;
+        }
+    }
+
+    if (isHit(x, y)) 
+    {
+        cout << "It's a hit!" << endl;
+        board[x][y] = 'X';
+        lifePoints--;
+    } 
+    else 
+    {
+        cout << "Missed!" << endl;
+        board[x][y] = 'O';
+    }
+
+}
+
+bool BattleshipBoard::isDead() 
+{
+    return lifePoints == 0;
+}
+
+int main() {
+    BattleshipBoard player1 = BattleshipBoard(1);
+    BattleshipBoard player2 = BattleshipBoard(2);
+
+    cout << "Player 1, place your ships:" << endl;
+    player1.placeShips();
+
+    cout << "Player 2, place your ships:" << endl;
+    player2.placeShips();
+
+    while (true) 
+    {
+        cout << "Player " << 1 << "'s Turn" << endl;
+        player1.displayBoard();
+        player1.playTurn();
+        if (player1.isDead()) 
+        {
+            cout << "Player 2 wins!" << endl;
+            break;
+        }
+
+        cout << "Player " << 2 << "'s Turn" << endl;
+        player2.displayBoard();
+        player2.playTurn();
+        if (player2.isDead()) 
+        {
+            cout << "Player 1 wins!" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
